ab3e9ba52717a19b5df1e79d76f3f2a173ba432a
dppereira <diogoppereira504@gmail.com>
2024-12-02 23:27:55 -0300

    Documentation lexer

--- lexer.c
+++ lexer.c
@@ -3,16 +3,26 @@
 #include <stdio.h>
 #include <string.h>
 
+// Variavel que guarda o valor numerico do ultimo token lido
 double lexval;
+
+// Contador de linhas, util para reportar erros ou mensagens relacionadas a posicao no codigo
 int linenum = 1;
+
+// Variavel global para armazenar o lexema atual (cadeia lida pelo analisador lexico)
 char lexeme[MAXIDLEN + 1];
 
 // ASGN = [:=]
 // Ex: var := 4
+// Identifica o token de atribuicao ":="
 int isASGN(FILE *tape) {
   lexeme[0] = getc(tape);
+
+  // Verifica se o inicio do operador ":="
   if (lexeme[0] == ':') {
     lexeme[1] = getc(tape);
+
+    // Confirma o operador ":="
     if (lexeme[1] == '=') {
       lexeme[2] = 0;
       return ASGN;
@@ -26,6 +36,8 @@ int isASGN(FILE *tape) {
 
 // ID = [A-Za-z][A-Za-z0-9]*
 // Ex: abacaXI123
+// Identifica identificadores seguindo o padrao ID 
+// Inclui reconhecimento especial para "quit" e "exit" (case-insensitive)
 int isID(FILE *tape) {
   int i = 0;
   lexeme[i] = getc(tape);
@@ -38,6 +50,7 @@ int isID(FILE *tape) {
     ungetc(lexeme[i], tape);
     lexeme[i] = 0;
 
+    // Converte o lexema para letras minusculas para comparacao
     char lowlex[sizeof(lexeme)];
     for (int j = 0; lexeme[j] != '\0'; j++) {
       lowlex[j] = tolower(lexeme[j]);
@@ -57,14 +70,17 @@ int isID(FILE *tape) {
 }
 
 // OCT = 0[0-7]+
-// Ex: 01 02 03 04 011
+// Ex: 01, 02, 03, 04, 011
+// Identifica numeros no formato OCT
 int isOCT(FILE *tape){
 	int i = 0;
 	lexeme[i] = getc(tape);
 
+  // Verifica se começa com '0'
 	if (lexeme[i] == '0'){
 		i++;
 		while (isdigit(lexeme[i] = getc(tape))){
+      // Verifica se corresponde a um digito OCT
 			if (lexeme[i] < '0' || lexeme[i] > '7'){
 				ungetc(lexeme[i], tape);
 				lexeme[i] = 0;
@@ -84,14 +100,18 @@ int isOCT(FILE *tape){
 }
 
 // HEX = 0[xX][0-9a-fA-F]+
-// Ex: 0X1 0X2 0x3 0x4 0x11
+// Ex: 0x1A, 0X2F
+// Identifica numeros no formato HEX
 int isHEX(FILE *tape){
 	int i = 0;
  	lexeme[i] = getc(tape);
 
+  // Verifica se começa com '0'
  	if (lexeme[i] == '0') {
 		i++;
  		if (!isspace(lexeme[i] = getc(tape))){
+      
+      // Confirmacao de hexadecimal
  			if (lexeme[i] == 'x' || lexeme[i] == 'X'){
 				i++;
  				while(isxdigit(lexeme[i] = getc(tape))) i++;
@@ -110,7 +130,11 @@ int isHEX(FILE *tape){
  	return 0;
 }
 
-// 
+// uint = [1-9][0-9]* | 0
+// Enotation = [eE]['+''-']?[0-9]+
+// float = (uint.[0-9]*|'.'[0-9]+)(Enotation)? | uint Enotation
+// NUM = uint | float
+// Identifica numeros no formato NUM
 int isNUM(FILE *tape) {
   int i = 0, j = 0;
   lexeme[i] = getc(tape);
@@ -138,6 +162,7 @@ int isNUM(FILE *tape) {
         i++;
       }
 
+      // Valida digitos depois do ponto
       if (j == i) {
         ungetc(lexeme[i], tape);
         lexeme[i] = 0;
@@ -145,12 +170,12 @@ int isNUM(FILE *tape) {
       }
     }
 
-    // Verifica Exponencial
+    // Verifica Enotation
     if (lexeme[i] == 'e' || lexeme[i] == 'E') {
       i++;
       lexeme[i] = getc(tape);
 
-      // Verifica sinal
+      // Verifica sinal do expoente
       if (lexeme[i] == '+' || lexeme[i] == '-') {
         i++;
       } else {
@@ -162,6 +187,7 @@ int isNUM(FILE *tape) {
         i++;
       }
 
+      // Faz a validacao
       if ((j == i || (!isspace(lexeme[i]) && lexeme[i] != '\n')) &&
           lexeme[i] != '+' && lexeme[i] != '-' && lexeme[i] != '*' &&
           lexeme[i] != '/' && lexeme[i] != ')') {
@@ -179,6 +205,7 @@ int isNUM(FILE *tape) {
   return 0;
 }
 
+// Ignora caracteres de espaco em branco no fluxo de entrada (incluindo espacos, tabs e novas linhas)
 void skipspaces(FILE *tape) {
   int head, prev_head = EOF;
 
@@ -199,6 +226,7 @@ void skipspaces(FILE *tape) {
   }
 }
 
+// Identifica e retorna token do fluxo de entrada
 int gettoken(FILE *source) {
   int token;
 
